f3b5df6baeef3048bd2228583733133b9ad4ce65
JacobWylie <jacobwylie@gmail.com>
2018-01-23 12:41:08 +0100

    populateCategoies spec created and passed

--- spec/populateCategories-spec.js
+++ spec/populateCategories-spec.js
@@ -22,14 +22,75 @@ describe("populate categories", () => {
 		this.img = img;
 		this.id = id
 	}
-	let newCat = new Category(catNames[0], productDetails[0].img, 1)	
+	let newCat = new Category(catNames[0], productDetails[0][catNames[0]], 0)	
 	it('should have an type', () => {
 		expect(newCat.type).toEqual(catNames[0])
 	})
 	it('should have an img url', () => {
-		expect(newCat.img).toEqual(productDetails[0].img)
+		expect(newCat.img).toEqual(productDetails[0][catNames[0]])
 	})
 	it('should have an id', () => {
-		expect(newCat.id).toEqual(1)
+		expect(newCat.id).toEqual(0)
 	})
+
+	const catArr = []
+	for (let i=0; i<1; i++) {
+		let newCat = new Category(catNames[i], productDetails[0][catNames[0]], i)
+		catArr.push(newCat)
+	}
+	it('should contain object data', () => {
+		expect(catArr[0].type).toEqual(catNames[0])
+	})
+	it('should contain object data', () => {
+		expect(catArr[0].img).toEqual(productDetails[0][catNames[0]])
+	})
+	it('should contain object data', () => {
+		expect(catArr[0].id).toEqual(0)
+	})
+
+	let pc = popCats();
+	it('should return an array with objects', () => {
+		expect(pc).toContain(jasmine.objectContaining(newCat))
+	})
+
 });
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
